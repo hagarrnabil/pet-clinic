821265ec4d5532de51251eeb13dac675843b7b39
hagar <hagarnabil7@gmail.com>
2023-11-29 15:51:10 +0200

    adding owners on owners page. Closes #16.

--- pet-clinic-web/src/main/java/guru/springframework/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/petclinic/controllers/OwnerController.java
@@ -1,12 +1,22 @@
 package guru.springframework.petclinic.controllers;
 
+import guru.springframework.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
+    private OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/owners", "owners/index", "owners/index.html"})
-    public String getOwners() {
+    public String getOwners(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
     }
