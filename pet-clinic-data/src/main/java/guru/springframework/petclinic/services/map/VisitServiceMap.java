03b3a3c8ecf907fb9b570e8f1ce247469915d89c
hagar <hagarnabil7@gmail.com>
2023-12-03 15:06:37 +0200

    created visit jpa service. Closes #46.

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/VisitServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/VisitServiceMap.java
@@ -2,9 +2,10 @@ package guru.springframework.petclinic.services.map;
 
 import guru.springframework.petclinic.model.Visit;
 import guru.springframework.petclinic.services.VisitService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
-
+@Service
 public class VisitServiceMap extends AbstractMapService<Visit,Long> implements VisitService {
     @Override
     public Set<Visit> findAll() {
