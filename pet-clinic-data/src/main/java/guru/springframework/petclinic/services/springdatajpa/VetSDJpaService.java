1e4e02f5dc565c9f62bb1cca176515e4ae3d82a5
hagar <hagarnabil7@gmail.com>
2023-12-03 14:33:20 +0200

    created pets jpa service. Closes #41.

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/services/springdatajpa/VetSDJpaService.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/springdatajpa/VetSDJpaService.java
@@ -3,10 +3,13 @@ package guru.springframework.petclinic.services.springdatajpa;
 import guru.springframework.petclinic.model.Vet;
 import guru.springframework.petclinic.repositories.VetRepository;
 import guru.springframework.petclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
-
+@Service
+@Profile("springdatajpa")
 public class VetSDJpaService implements VetService {
     private final VetRepository vetRepository;
 
