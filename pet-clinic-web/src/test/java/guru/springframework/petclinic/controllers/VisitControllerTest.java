b34ea3f0e47f9f7da247ca4f0c3bac4c3f0d4762
hagar <hagarnabil7@gmail.com>
2023-12-06 01:59:44 +0200

    add and update visit. Closes #56.

---
+++ pet-clinic-web/src/test/java/guru/springframework/petclinic/controllers/VisitControllerTest.java
@@ -0,0 +1,29 @@
+package guru.springframework.petclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VisitControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void dataBinder() {
+    }
+
+    @Test
+    void loadPetWithVisit() {
+    }
+
+    @Test
+    void initNewVisitForm() {
+    }
+
+    @Test
+    void processNewVisitForm() {
+    }
+}
